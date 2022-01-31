495f0a45c8ab57c7609b638a840e0acae7ca0fe6
cspdevs000 <cspdevs000@gmail.com>
2022-01-31 02:13:57 -0500

    deploying to heroku

--- neighborlyapp/settings.py
+++ neighborlyapp/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -137,3 +138,6 @@ MEDIA_URL = '/media/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = '/home'
+
+# Configure Django App for Heroku.
+django_heroku.settings(locals())
